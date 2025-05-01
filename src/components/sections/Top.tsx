62aefe14a79e024248abe14fa67a61350627bf36
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-05-01 20:52:01 +0900

    feat: add animation to top section

--- src/components/sections/Top.tsx
+++ src/components/sections/Top.tsx
@@ -1,18 +1,39 @@
+"use client";
 import React from "react";
-import "@/styles/top.scss";
+import { motion } from "framer-motion";
+import styles from "@/styles/top.module.scss";
 
 const Top: React.FC = () => {
   return (
-    <section id="top" className="top">
-      <h1 className="text-4xl font-bold mb-20">こんにちは！</h1>
-      <p className="text-lg mb-10">
+    <section id="top" className={styles.top}>
+      <motion.h1
+        className={styles.title}
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        こんにちは！
+      </motion.h1>
+
+      <motion.p
+        className={styles.paragraph}
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, delay: 0.3 }}
+      >
         春日晴都と申します。<br />
         自己紹介ページをご覧いただき、ありがとうございます。<br />
         現在、Webエンジニアリングの学習をしながら、SE転職を目指しています。<br />
-      </p>
-      <p className="text-lg mb-10">
+      </motion.p>
+
+      <motion.p
+        className={styles.paragraph}
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.2, delay: 0.7 }}
+      >
         「下へスクロール」or「ヘッダーのリンク」より各紹介をご確認ください。<br />
-      </p>
+      </motion.p>
     </section>
   );
 };

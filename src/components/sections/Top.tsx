fed3808eb8dea7188186f0b0442b1a78f33c3812
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-05-21 06:36:45 +0900

    feat: update text & add scroll indicator in top section

--- src/components/sections/Top.tsx
+++ src/components/sections/Top.tsx
@@ -12,7 +12,7 @@ const Top: React.FC = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
       >
-        こんにちは！
+        春日晴都と申します
       </motion.h1>
 
       <motion.p
@@ -21,8 +21,7 @@ const Top: React.FC = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.2, delay: 0.3 }}
       >
-        春日晴都と申します。<br />
-        自己紹介ページをご覧いただき、ありがとうございます。<br />
+        自己紹介ページをご覧いただき、ありがとうございます。
       </motion.p>
 
       <motion.p
@@ -31,8 +30,28 @@ const Top: React.FC = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.2, delay: 0.7 }}
       >
-        「下へスクロール」or「ヘッダーのリンク」より各紹介をご確認ください。<br />
+        「下へスクロール」or「ヘッダーのリンク」より各紹介をご確認いただけます。
       </motion.p>
+
+      <motion.a
+        href="#selfintro"
+        className={styles["scroll-indicator"]}
+        initial={{ opacity: 0, y: -30 }}                              // 初期位置
+        animate={{
+          opacity: [0, 1, 1, 0],                                      // 透明 -> 表示 -> 表示維持 -> 透明
+          y: [0, 15, 15, 0],                                          // 最初の位置 -> 下 -> 下維持 -> 最初の位置に戻る (透明な状態で)
+        }}
+        transition={{
+          delay: 1.7,
+          duration: 2.2,
+          ease: "easeInOut",                                          // 始まりと終わりを滑らかに表示
+          repeat: Infinity,                                           // 無限に繰り返す
+          times: [0, 0.4, 0.8, 1],                                    /* 各キーフレームの割合 (0%から100%まで)
+                                                                      アニメーションの特定のポイント（キーフレーム）が、アニメーションの総時間に対してどの割合で発生するかを指定*/
+        }}
+      >
+        <span className={styles["arrow-down"]}></span>
+      </motion.a>        
     </section>
   );
 };

6d2a75fa90b0eafa4fc6d747e863501724090691
Haruto Kasuga <harutokasuga.610@gmail.com>
2025-07-08 22:17:38 +0900

    edit: timeline, page, layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import BusinessCard from "@/components/sections/BusinessCard";
-import SelfIntro from "@/components/sections/SelfIntro";
-import Timeline from "@/components/sections/Timeline";
-import Portfolio from "@/components/sections/Portfolio";
-import Contact from "@/components/sections/Contact";
 
 const Home: React.FC = () => {
   return (
     <main>
       <BusinessCard />
-      <SelfIntro />
-      <Timeline />
-      <Portfolio />
-      <Contact />
     </main>
   );
 };
